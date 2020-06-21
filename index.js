245e2898069dbef587c009509db6e4a93b19e3c3
Zeeshan <zeeshanmehdi18@gmail.com>
2020-06-21 10:30:45 +0500

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+// import express (after npm install express)
+const express = require('express');
+const bodyParser = require("body-parser")
+
+// create new express app and save it as "app"
+const app = express();
+
+app.use(bodyParser.json())
+
+// server configuration
+const PORT = 8080 || process.env.PORT;
+
+var val = 'No value yet';
+
+// create a route for the app
+app.get('/', (req, res) => {
+    res.send(val);
+});
+
+app.post('/save', (req, res) => {
+    val = req.body;
+    res.status(200).end()
+});
+
+// make the server listen to requests
+app.listen(PORT, () => {
+    console.log(`Server running at: http://localhost:${PORT}/`);
+});
